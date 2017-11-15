e0898f2e5cd35c923c78ae5c167975ea076677b8
Vicky <vicky@vickylai.io>
2017-11-15 13:41:46 -0500

    Attempted callback solution...

---
+++ assets/js/game.js
@@ -0,0 +1,68 @@
+/*
+SIMON
+onload():
+- generate game array
+- play intro function - user chooses strict or normal
+
+click():
+- input drives gameplay for loop
+*/
+
+// Globals
+var game = [];
+var strict;
+
+// Generate game array
+function makeGame() {
+    for (var i = 0; i <= 20; i++) {
+        var v = Math.ceil(Math.random() * 4);
+        game.push(v);
+    }
+    return game;
+}
+// 👍 console.log(makeGame());
+
+function simonSays() {
+    $('#orb' + game[step]).addClass() // Hover state?
+}
+
+function getInput() {
+    // Get value of clicked orb
+    $('.orb').click(function (event) {
+        var input = parseInt($(this).attr('value'));
+        console.log('input = ' + input);
+        return input;
+    })
+}
+
+// Called in game for loop where step is i, click target is input
+function compare(step, input) {
+    //return game[step] === input;
+    return verdict = compare(input == );
+}
+
+// Callback
+function response(verdict) {
+    if (verdict) {
+        console.log('Good human.');
+    }
+    else {
+        console.log('Bad human.');
+    }
+}
+
+function processInput(callback) {
+    console.log('Choose.');
+    var input = getInput();
+    callback(compare);
+} // Shoud return true/false
+
+// Page loaded
+$(document).ready(function () {
+    console.log('Want to play a game?');
+    // make game, intro
+    // strict is true or false
+
+    processInput(response)
+
+});
