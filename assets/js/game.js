df633f75b62b7afad3a86a979b0b6cf05234982a
Vicky <vicky@vickylai.io>
2017-11-15 19:27:07 -0500

    Made orbs glowy. Changing logic approach.

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,15 @@ onload():
 
 click():
 - input drives gameplay for loop
+
+nuke:
+- rumble effect
+- emanating glow to white screen
+- "Oh no..."
 */
 
 // Globals
-var game = [];
-var strict;
+var strict = false;
 
 // Generate game array
 function makeGame() {
@@ -18,42 +22,61 @@ function makeGame() {
         var v = Math.ceil(Math.random() * 4);
         game.push(v);
     }
+    console.log(game);
     return game;
-} // 👍 console.log(makeGame());
+}
 
-function simonSays(which) {
+function simonSays(sequence) {
+    for (var i=0; i<sequence.length; i++) {
+    console.log('Which orb: ' + sequence[i]);
     $('#orb' + which).addClass('notice');
     setTimeout(() => {
         $('#orb' + which).removeClass('notice');
     }, 350);
+    }
 } // 👍
 
+function nuke() {
+    console.log("Kill with fire.");
+}
 
 function dontGetInput() { // no listener for you
     $('.orb').off("click");
 }
 
-function getInput(step) { // adds event listener
-    // Get value of clicked orb
-    $('.orb').click(function (event) {
-        var input = parseInt($(this).attr('value'));
-        // Log value
-        console.log('input = ' + input);
-        compare(step, input)
-    });
+function getInput(orb) {
+        // Get value of clicked orb
+        $('.orb').click(function (event) { // Click drives gameplay
+            var input = parseInt($(this).attr('value'));
+            // Log value
+            console.log('input = ' + input);
+            compare(orb, input);
+        });
 }
 
 // Called in game for loop where step is i, click target is input
-function compare(step, input) {
-    var verdict = step === input;
-
+function compare(orb, input) {
+    var verdict = orb === input;
+    console.log('verdict: ' + verdict);
+    if (verdict === false && strict) {
+        nuke();
+    }
+    if (verdict === false && strict === false) {
+        dontGetInput();
+        // Feedback
+        console.log('Bad human.');
+    }
     if (verdict) {
         console.log('Good human.');
     }
-    else {
-        console.log('Bad human.');
-        dontGetInput();
-    }
+}
+
+// One step
+function play(step) {
+    console.log('step: ' + step);
+    console.log('orb: ' + game[step]);
+    simonSays(game[step]);
+    getInput(game[step]);
 }
 
 // Page loaded
@@ -63,10 +86,6 @@ $(document).ready(function () {
     makeGame();
     // strict is true or false
 
-    for (var i = 0; i < 1; i++) {
-        console.log(game[i]);
-        simonSays(game[i]);
-        getInput(game[i]);
-    }
+
 
 });
