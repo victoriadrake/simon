0f218d2458dfd09b2ceddb4270a11de5942c5f31
Vicky <vicky@vickylai.io>
2017-11-17 14:02:50 -0500

    More finishing touches...

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,19 +14,39 @@ var sounds = {
 };
 
 var msg = {
-        1: 'Four mysterious floating orbs have appeared!',
-        2: 'From all around you, a voice is heard...',
-        3: 'Simon: Greetings, traveler. I am Simon, a highly advanced artificial intelligence written in Clojure. I am the keeper of this gateway.',
-        4: 'To pass, you must first prove yourself worthy by accepting my challenge! It is a formidable test of skills based on a children\'s toy from the ancient era known only as "the eighties."',
-        5: 'Simon: Follow the sequence of the orbs. Master twenty steps and you shall pass; fail and you risk certain doom!',
+    0: 'Wandering through the post-apocalyptic city of Quin\'see, you suddenly find your path blocked...',
+    1: 'Four mysterious floating orbs have appeared!',
+    2: 'From all around you, a voice is heard...',
+    3: 'Simon: Greetings, traveler. I am Simon, a highly advanced artificial intelligence written in Clojure. I am the keeper of this gateway.',
+    4: 'Simon: To pass, you must first prove yourself worthy by accepting my challenge! It is a formidable test of skills based on a children\'s toy from the ancient era known only as "The Eighties."',
+    5: 'Simon: Follow the sequence of the orbs. Master twenty steps and you shall pass; fail and you risk certain doom!',
+}
+
+var simonPrompt = {
+    0: 'Simon: My turn.',
+    1: 'Simon: Pay close attention now.',
+    2: 'Simon: Concentrate.',
 }
 
 function intro() {
-    $('#message').html(msg[1]); // click to advance
+    let i = 1;
+    $('#message').html(msg[0]); // click to advance
+    $('#next').click(function () {
+        console.log(msg[i]);
+        if (i < 5) {
+            $('#message').html(msg[i++]);
+            $('#floor').removeClass('empty');
+        } else {
+            $('#next').css('opacity', '0');
+            $('#message').html(msg[i++]);
+            $('.controls').css('opacity', '1');
+        }
+    });
 }
 
 function noticeMe(orb) {
     $('#orb' + orb).addClass('notice');
+    console.log('notice!');
     switch (orb) {
         case 1: sounds.red.play(); break;
         case 2: sounds.green.play(); break;
@@ -35,7 +55,7 @@ function noticeMe(orb) {
     }
     setTimeout(() => {
         $('#orb' + orb).removeClass('notice');
-    }, 350);
+    }, 400);
 }
 
 var simon = {
@@ -47,6 +67,9 @@ var simon = {
         $('#stage').html(game.stage);
     },
     playSimon: function () { // could add no-click here?
+        var m = Math.ceil(Math.random() * (Object.keys(simonPrompt).length - 1));
+        console.log(simonPrompt[m]);
+        $('#message').html(simonPrompt[m]);
         var v = 0;
         var t = setInterval(function () {
             noticeMe(game.values[v]);
@@ -57,7 +80,7 @@ var simon = {
             else {
                 v++;
             }
-        }, 500)
+        }, 700)
     }
 };
 
@@ -77,7 +100,7 @@ var player = {
             if (player.step >= player.total) {
                 game.setState(player);
                 console.log('Good human.');
-                $('#message').html('Simon: Good.');
+                //$('#message').html('Simon: Good.');
             }
         }
         else {
@@ -116,7 +139,7 @@ var setMode = {
             $('#message').html('Simon: ...');
         }, 2750);
         setTimeout(() => {
-            $('.replay').css('display','block');
+            $('#replay').css('display', 'block');
         }, 4000);
         // jQuery, css stuff
         // show option to game.start()
@@ -131,7 +154,7 @@ var game = {
     reset: function () {
         game.stage = 0;
         game.state = simon;
-        if (game.mode == 'normal'){
+        if (game.mode == 'normal') {
             setMode.normal()
         }
         if (game.mode == 'strict') {
@@ -143,7 +166,7 @@ var game = {
     start: function () {
         $('#floor').removeClass('empty');
         $('.controls').removeClass('dim');
-        $('.replay').css('display','none');
+        $('#replay').css('display', 'none');
         $('#message').html('Simon: Let us begin.');
         game.reset();
         $('.orb').addClass('no-click');
@@ -158,7 +181,7 @@ var game = {
         else {
             game.state = simon;
             $('.orb').addClass('no-click');
-            if (game.stage >= 20) {
+            if (game.stage >= 5) {
                 game.win(); //win?
             }
             else {
@@ -182,11 +205,30 @@ var game = {
             setTimeout(() => {
                 $('#floor').removeClass('quake');
                 simon.playSimon();
-            }, 400);
+            }, 500);
         }
     },
     win: function () {
-        $('#message').html('Simon: Well done, brave human.');
+        $('#message').html('Simon: Well done, brave human. Safe passage is yours.');
+        //all flash
+        $('.wrapper').addClass('happy-time'); // transition?
+        $('.controls').addClass('dim');
+        let orbs = [3,4,4,3,4,2,1];
+        let v = 0;
+        let t = setInterval(function () {
+            noticeMe(orbs[v]);
+            if (v >= orbs.length - 1) {
+                clearInterval(t);
+            }
+            else {
+                v++;
+            }
+        }, 700)
+        setTimeout (() => {
+            $('.wrapper').removeClass('happy-time');
+            $('#floor').addClass('empty');
+            $('#replay').css('display', 'block');
+        }, 5300)
         // jQuery, css stuff
         // show option to game.start()
     }
@@ -198,7 +240,6 @@ $(document).ready(function () {
         sounds[key].load();
     });
 
-    $('.controls').css('opacity','0');
     intro();
 
     $('.orb').click(function (event) {
@@ -220,7 +261,7 @@ $(document).ready(function () {
         $('#start').html('Restart');
     });
 
-    $('.replay').click(function() {
+    $('#replay').click(function () {
         game.start();
     })
 
